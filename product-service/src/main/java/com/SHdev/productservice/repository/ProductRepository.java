a73e00509c5fba25e799d1de668893ca29f05855
Shihara Jayainghe <shiharadilshan13@gmail.com>
2024-04-09 16:59:29 +0530

    clearance

--- product-service/src/main/java/com/SHdev/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.SHdev.productservice.repository;
-
-import com.SHdev.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
