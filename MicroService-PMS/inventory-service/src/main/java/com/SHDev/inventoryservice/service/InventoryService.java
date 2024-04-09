dc4e6ae3663e2c48d6b4f47b689313f42ee59b54
Shihara Jayainghe <shiharadilshan13@gmail.com>
2024-04-09 18:37:17 +0530

    added communication between inventory and order services

--- MicroService-PMS/inventory-service/src/main/java/com/SHDev/inventoryservice/service/InventoryService.java
+++ MicroService-PMS/inventory-service/src/main/java/com/SHDev/inventoryservice/service/InventoryService.java
@@ -1,5 +1,6 @@
 package com.SHDev.inventoryservice.service;
 
+import com.SHDev.inventoryservice.dto.InventoryResponse;
 import com.SHDev.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,13 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode){
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode){
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0)
+                            .build()
+                ).toList();
     }
 }
