db377512c6c78689066dc7965d8eb7c8c632cf32
Shihara <shiharadilshan13@gmail.com>
2024-05-05 01:32:49 +0530

    fixed sql connection

--- MicroService-PMS/inventory-service/src/main/java/com/SHDev/inventoryservice/InventoryServiceApplication.java
+++ MicroService-PMS/inventory-service/src/main/java/com/SHDev/inventoryservice/InventoryServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
